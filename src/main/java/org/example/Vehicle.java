8be4e1fdef0953d70ee074c712c18d4699e4b688
Adrian <addyionut@gmail.com>
2023-01-27 00:52:13 +0200

    declaring methods

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 public class Vehicle {
     String name;
     double fuelLevel;
@@ -10,4 +9,19 @@ public class Vehicle {
     boolean damaged;
     String color;
     int price;
+
+    public double accelerate(double speed, double durationInHours) {
+        System.out.println(name + "is accelerating with " + speed + "km/h for " + durationInHours + " h");
+        double distance = speed * durationInHours;
+        totalTraveledDistance += distance;
+
+        System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
+        double usedFuel = distance * mileage / 100;
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel);
+
+        return distance;
+    }
+
+
 }
